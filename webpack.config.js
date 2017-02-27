52f7b572e4956c9a0365c2e9569bc96d9704dd34
Justin Wood <jwood11atx@gmail.com>
2017-02-27 02:08:00 -0700

    finish recommendation logic and api call chains

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
         test: /\.jsx?/,
         loader: "babel-loader",
         exclude: /node_modules/,
+        query:
+        {
+          presets:["react", "es2015"]
+        }
       },
       { test: /\.css$/, loader: 'style!css' },
       { test: /\.scss$/, loader: 'style!css!sass' },

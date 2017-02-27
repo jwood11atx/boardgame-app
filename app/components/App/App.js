186d66911c80127bbbd2f3efaada068ae21b567e
Justin Wood <jwood11atx@gmail.com>
2017-02-27 02:26:45 -0700

    clean up commented out code

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -51,76 +51,19 @@ class App extends React.Component {
     }
     return arr;
   }
-//do request to express server
-//(express)(routes comes with express i think) set routes namespaced like this: /api/v1/*
-//example localhost:3000/api/v1/recommendations?query=catan,pandemic,splendor,resistance
-//this route will send requests to a particular controller. that controller then handles the data manipulation and API calls to boardgamegeek. Once the data has been figured out, you will take that, convert it to JSON, and then return that data.
-
-//you are going to want to pull in a libary to handle cors. follow the directions in the libary readme and it'll tell you how to set the cors headers accordingly. shouldn't be too hard.
-
-//you still need firebase, but your backend/express app will the one communicating with your firebase db.
-
 
   getGames(){
-      var ids = this.state.userBGListIDs.join(",");
-      var app = this;
-      var count = 0;
-      var list = []
+    var ids = this.state.userBGListIDs.join(",");
+    var app = this;
+    var count = 0;
+    var list = []
 
-      return fetch(`http://localhost:3000/boardgames?id=${ids}`)
-        .then(res => res.json())
-        .then(userBGList => {
-          // userBGList.forEach((e, i) => {
-            // count++;
-            // if(e["xml"]){
-            //   fetch(`http://localhost:3000/xml?id=${e["xml"]}`)
-            //     .then(res => res.json())
-            //     .then(data => {
-            //       userBGList[i] = data;
-            //       // this.setState({userBGList})
-            //     })
-            // }
-          // })
-          // return userBGList;
-          this.setState({userBGList})
-        })
-        // .then(function(userBGList){
-        //   if (count === userBGList.length) {
-        //     list = userBGList;
-        //     app.setState({userBGList});
-        //   }
-        // })
-        return list;
-      // if(this.state.userBGList.length > 0){
-      //   console.log(this.state.userBGList);
-      // }
-
-    // this.getMatches();
-
-
-
-
-
-
-    // fetch("https://www.boardgamegeek.com/xmlapi2/thing?id=110308,13", {
-      // })
-      // .then(text => {
-      //   const parser = new DOMParser();
-      //   console.log("text", text);
-      //   console.log("parser", parser);
-      //   console.log("parser result", parser.parseFromString(text, "text/xml"));
-      //   return parser.parseFromString(text, "text/xml");
-      // })
-      // .then(xml => console.log(xml))
-      // .then(htmlText => {
-        // console.log(htmlText)
-        // console.log("designers", data.designers);
-        // console.log("artists", data.artists);
-        // console.log("publishers", data.publishers);
-        // console.log("categories", data.categories);
-        // console.log("mechanics", data.mechanics);
-        // console.log("family", data.family);
-      // })
+    return fetch(`http://localhost:3000/boardgames?id=${ids}`)
+      .then(res => res.json())
+      .then(userBGList => {
+        this.setState({userBGList})
+      })
+    return list;
   }
 
   getXML(){
@@ -146,7 +89,7 @@ class App extends React.Component {
       app.getXML()
       setTimeout(()=>{
         resolve();
-      }, 500)
+      }, 1000)
       })
 
       promise.then((test) => {
@@ -169,10 +112,6 @@ class App extends React.Component {
 
 
   }
-
-// var recommendationObj = recommendations([fakeEntry.stubENTRY1, fakeEntry.stubENTRY2]);
-//
-// console.log(recommendationObj);
 
   getMatches(){
     this.setState({matchList: []})

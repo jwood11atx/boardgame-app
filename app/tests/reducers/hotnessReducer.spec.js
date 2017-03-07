ed58866978380b56238d41e0780f073819d7ee32
Justin Wood <jwood11atx@gmail.com>
2017-03-07 15:52:11 -0600

    move hotness test into test folder. add displayed reducer tests.

--- app/tests/reducers/hotnessReducer.spec.js
+++ app/tests/reducers/hotnessReducer.spec.js
@@ -1,20 +1,18 @@
-import hotenessReducer from '../../reducers/hotness-reducer';
-import {expect} from 'chai';
+import hotenessReducer from "../../reducers/hotness-reducer";
+import {expect} from "chai";
 
+describe("hotness-reducer", () => {
 
-describe('hotness-reducer', () => {
-
-  it('should return default state of an empty array', () => {
-    expect(hotenessReducer(undefined, {})).toEqual([])
+  it("should return default state of an empty array", () => {
+    expect(hotenessReducer(undefined, {})).eql([])
   })
 
-  it('should return an array equal to the length of action.movies if LOAD_MOVIES is the action type', () => {
+  it("should return an array equal to the length of action.hotness if GET_HOTNESS is the action type", () => {
     const hotness = [{id: "13", title: "catan"}, {id: "1234", title: "test"}];
     const action = {
       type: "GET_HOTNESS",
       hotness
     }
-    expect(hotenessReducer(undefined, action)).toHaveLength(3);
+    expect(hotenessReducer(undefined, action)).to.have.lengthOf(2);
   })
-
 })
